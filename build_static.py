f9d1f1b0c872e312cac45508dabce05ad85a03a4
patdum35 <35080841+patdum35@users.noreply.github.com>
2025-01-24 22:21:47 +0100

    Add files via upload

--- build_static.py
+++ build_static.py
@@ -1,22 +1,107 @@
+# from app import GenealogyViewer
+# import os
+# import shutil
+# import json
+
+# def build_static():
+#     app = GenealogyViewer()
+    
+#     os.makedirs('build', exist_ok=True)
+    
+#     # Copie fichier de données
+#     shutil.copy2('arbre.enc', 'build/')
+    
+#     # Génère index.html avec tous les composants
+#     html = app.app.index()
+    
+#     # Sauvegarde
+#     with open('build/index.html', 'w', encoding='utf-8') as f:
+#         f.write(html)
+
+# if __name__ == '__main__':
+#     build_static()
+
+
+########################################
+
+# from app import GenealogyViewer
+# import os
+# import shutil
+
+# def build_static():
+#     app = GenealogyViewer()
+    
+#     os.makedirs('build', exist_ok=True)
+    
+#     # Copie fichier de données
+#     shutil.copy2('arbre.enc', 'build/')
+    
+#     # Génération HTML statique complète
+#     static_html = f"""
+#     <!DOCTYPE html>
+#     <html lang="fr">
+#     <head>
+#         <meta charset="UTF-8">
+#         <title>Arbre Généalogique</title>
+#         <script src="https://unpkg.com/dash-html-components@2.0.0/dash_html_components/bundle.js"></script>
+#         <script src="https://unpkg.com/dash-core-components@2.0.0/dash_core_components/bundle.js"></script>
+#         <script src="https://unpkg.com/dash-cytoscape@0.3.0/dash_cytoscape/bundle.js"></script>
+#         <link rel="stylesheet" href="https://codepen.io/chriddyp/pen/bWLwgP.css">
+#     </head>
+#     <body>
+#         {app.app.layout}
+#         <script>
+#             // Ajoutez ici tout script nécessaire pour l'interactivité
+#         </script>
+#     </body>
+#     </html>
+#     """
+    
+#     # Sauvegarde
+#     with open('build/index.html', 'w', encoding='utf-8') as f:
+#         f.write(static_html)
+    
+#     print("Fichiers dans build:", os.listdir('build'))
+
+# if __name__ == '__main__':
+#     build_static()
+
+#######################################
+
 from app import GenealogyViewer
 import os
 import shutil
-import json
 
 def build_static():
-    app = GenealogyViewer()
-    
     os.makedirs('build', exist_ok=True)
     
     # Copie fichier de données
     shutil.copy2('arbre.enc', 'build/')
     
-    # Génère index.html avec tous les composants
-    html = app.app.index()
+    # Instance de l'application
+    app = GenealogyViewer()
     
-    # Sauvegarde
+    # Générer un fichier HTML minimal
+    html_content = f"""
+    <!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Arbre Généalogique</title>
+        <script src="https://unpkg.com/dash-html-components/dash_html_components/bundle.js"></script>
+        <script src="https://unpkg.com/dash-core-components/dash_core_components/bundle.js"></script>
+        <script src="https://unpkg.com/dash-cytoscape/dash_cytoscape/bundle.js"></script>
+    </head>
+    <body>
+        {str(app.app.layout)}
+    </body>
+    </html>
+    """
+    
     with open('build/index.html', 'w', encoding='utf-8') as f:
-        f.write(html)
+        f.write(html_content)
+    
+    print("Fichiers dans build:", os.listdir('build'))
 
 if __name__ == '__main__':
     build_static()
